7e24eac914dbd3ee682598b0c032410e1950a6c4
Bram Borggreve <borggreve@gmail.com>
2014-02-07 17:46:28 +0100

    MEAN project branded to dnschanged

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.users']);
+
+angular.module('mean.system', []);
+angular.module('mean.articles', []);
+angular.module('mean.users', []);
